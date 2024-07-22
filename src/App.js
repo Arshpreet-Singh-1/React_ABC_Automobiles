621d6bd9ce34d49328cd270cb03d2dbba4e21204
Arshpreet-Singh-1 <asingh26_be20@thapar.edu>
2024-07-22 11:55:45 +0530

    adding forms using formik and yup

--- src/App.js
+++ src/App.js
@@ -24,16 +24,50 @@ they are more powerful than functional components but also more complex
 
 */
 
-import Menu from "./Menu";
-import About from "./About";
-import Footer from "./Footer";
+/*
+Routing 
+
+1. Install React Router
+npm install react-router-dom
+
+2. Set Up Browser Router
+update src/index.js to include BrowserRouter
+update src/App.js to include routes
+update src/Menu.js to provide navigation links 
+
+
+*/
+
+
+/*
+JSON SERVER 
+
+1. Install json-server
+npm install -g json-server
+
+2. Start the json-server
+json-server --watch db.json --port 5000
+
+*/
+
+import Menu from "./components/Menu";
+import About from "./components/About";
+import Footer from "./components/Footer";
+import Home from "./components/Home";
+import { Route,Routes } from "react-router-dom";
+import VehicleList from "./components/VehicleList";
+
 
 
 function App(){
   return (
     <>
     <Menu/>
-    <About/>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/about" element={<About/>}/>
+      <Route path="/vehicle-list" element={<VehicleList/>}/>
+    </Routes>
     <Footer/>
     </>
   )
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,28 +1,31 @@
+import {Link} from 'react-router-dom';
+
+
 function Menu(){
     return (
       <>
       <nav className="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
   <div className="container-fluid">
-    <a className="navbar-brand" href="#">ABC Automobiles</a>
+    <Link className="navbar-brand" to="/">ABC Automobiles</Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0" data-testid="menu-content">
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Home</a>
+        <Link className="nav-link" to="/">Home</Link>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">About Us</a>
+          <Link className="nav-link" to="/about">About Us</Link>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">Vehicles</a>
+        <Link className="nav-link" to="/vehicle-list">Vehicles</Link>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">Services</a>
+        <Link className="nav-link" to="/about">Services</Link>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">Contact</a>
+        <Link className="nav-link" to="/about">Contact</Link>
         </li>
         
       </ul>
